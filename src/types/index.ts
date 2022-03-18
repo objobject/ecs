6a6a693d24780f770b6afb5082f2afa57378932a
objobject <frytek97@gmail.com>
2022-03-18 12:35:29 +0100

    extend flatten value types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
-export type Values<T> = T[keyof T]
-export type Primitive = string | number | boolean
-export type FlattenPairs<T> = {[K in keyof T]: T[K] extends Primitive ? [K, T[K]] : FlattenPairs<T[K]>}[keyof T] & [PropertyKey, Primitive]
+export type Values<T> = T[keyof T];
+export type FlattenValue = string | number | boolean | Function;
+export type FlattenPairs<T> = {[K in keyof T]: T[K] extends FlattenValue ? [K, T[K]] : FlattenPairs<T[K]>}[keyof T] & [PropertyKey, FlattenValue];
 export type Flatten<T> = {[P in FlattenPairs<T> as P[0]]: P[1]}
 
 export interface ComponentI<T extends Record<string, unknown> = {}> {
