7a654e79b0749e6e451ca98f9972c3b0f13e47fa
objobject <frytek97@gmail.com>
2022-03-19 11:17:01 +0100

    add object to FlattenValue

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 export type Values<T> = T[keyof T];
-export type FlattenValue = string | number | boolean | Function;
+export type FlattenValue = string | number | boolean | Function | object;
 export type FlattenPairs<T> = {[K in keyof T]: T[K] extends FlattenValue ? [K, T[K]] : FlattenPairs<T[K]>}[keyof T] & [PropertyKey, FlattenValue];
 export type Flatten<T> = {[P in FlattenPairs<T> as P[0]]: P[1]}
 
